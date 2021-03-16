acde82160cec3c3d4c562a89fb3622eed8eb4e09
Leon <hualiang.tian@gmail.com>
2021-03-15 20:54:19 -0400

    commit tree e1a26a3794ea99940555d612c51ba852d8dab1d7

---
+++ lib/song.rb
@@ -0,0 +1,60 @@
+require 'pry'
+
+class Song
+@@count = 0
+@@genres = []
+@@artists = [] 
+ 
+attr_accessor :name, :artist, :genre 
+attr_reader :count, :artists, :genres, :genre_count, :artist_count 
+def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << self.artist
+    @@genres << self.genre
+end
+
+def self.count
+    @@count
+end
+
+def self.artists
+    @@artists.uniq
+end
+
+def self.genres
+    @@genres.uniq
+end
+
+def self.genre_count
+# binding.pry
+genre_count = {}  
+
+    @@genres.each do |genre|
+        if !genre_count[genre]
+         genre_count[genre] = 1
+         else
+         genre_count[genre]+=1
+         end
+        end
+        genre_count
+
+
+end
+
+def self.artist_count
+    artist_count = {}
+
+    @@artists.each do |artist|
+        if !artist_count[artist]
+         artist_count[artist] = 1
+         else
+         artist_count[artist] += 1
+         end
+        end
+    artist_count
+end
+
+end
